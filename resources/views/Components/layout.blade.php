589b7ef2612dc771dbacd7ad07828d750d1932c8
irfan87 <nerve2009@yahoo.com>
2024-08-05 11:25:22 +0800

    nav link blade for mobile view

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -69,9 +69,9 @@
       <div class="md:hidden" id="mobile-menu">
         <div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
           <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-          <a href="/" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</a>
-          <a href="/jobs" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Jobs</a>
-          <a href="/contact" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+          <x-nav-link href="/" :active="request()->is('/')">Home</x-nav-link>
+          <x-nav-link href="/jobs" :active="request()->is('jobs')">Jobs</x-nav-link>
+          <x-nav-link href="/contact" :active="request()->is('contact')">Contact</x-nav-link>
         </div>
         <div class="border-t border-gray-700 pb-3 pt-4">
           <div class="flex items-center px-5">
